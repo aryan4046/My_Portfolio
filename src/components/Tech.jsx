b149796b79641f8036e812c49f859f80f679a8cf
aryan4046 <aryansamani4046@gmail.com>
2026-01-22 01:02:52 +0530

    small update

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,12 +6,18 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+    <div className="max-w-7xl mx-auto px-4">
+      {/* Responsive Grid */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 justify-items-center">
+        {technologies.map((technology) => (
+          <div
+            key={technology.name}
+            className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28"
+          >
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
