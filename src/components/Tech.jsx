310b61b2d1854b5f6e2e63116a2ff57e879ae5af
aryan4046 <aryansamani4046@gmail.com>
2026-02-19 23:38:32 +0530

    Make More Mobile Friendly

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import useIsMobile from "../hooks/useIsMobile";
 
 const Tech = () => {
+  const isMobile = useIsMobile();
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+        <div className="w-28 h-28 flex justify-center items-center" key={technology.name}>
+          {isMobile ? (
+            <img
+              src={technology.icon}
+              alt={technology.name}
+              className="w-20 h-20 object-contain"
+              loading="lazy"
+            />
+          ) : (
+            <BallCanvas icon={technology.icon} />
+          )}
         </div>
       ))}
     </div>
--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,5 @@
 import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
 
 import InteractiveScene from "./InteractiveScene";
@@ -12,4 +11,4 @@ import HolographicLaptopCanvas from "./HolographicLaptop";
 import HolographicDashboardCanvas from "./HolographicDashboard";
 import AntigravityBackground from "./AntigravityBackground";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, InteractiveScene, FunnyRobotCanvas, TechSphereCanvas, AiNeuralCoreCanvas, HolographicLaptopCanvas, HolographicDashboardCanvas, AntigravityBackground, RobotCanvas };
+export { EarthCanvas, BallCanvas, StarsCanvas, InteractiveScene, FunnyRobotCanvas, TechSphereCanvas, AiNeuralCoreCanvas, HolographicLaptopCanvas, HolographicDashboardCanvas, AntigravityBackground, RobotCanvas };
